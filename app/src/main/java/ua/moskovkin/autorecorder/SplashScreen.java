1cba1ca3d27910ecbae80247e2dda9172a3b125f
Wraith007 <pisma_yest@ukr.net>
2016-03-08 22:50:16 +0200

    AsyncTask Added

--- app/src/main/java/ua/moskovkin/autorecorder/SplashScreen.java
+++ app/src/main/java/ua/moskovkin/autorecorder/SplashScreen.java
@@ -3,6 +3,7 @@ package ua.moskovkin.autorecorder;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
@@ -15,7 +16,7 @@ import java.util.GregorianCalendar;
 import ua.moskovkin.autorecorder.utils.DBHelper;
 
 public class SplashScreen extends Activity{
-    private static final int SPLASH_TIME_OUT = 2000;
+    private static final int SPLASH_TIME_OUT = 1000;
     private final static int PASS_REQUEST = 12;
     private File appFolder;
     private DBHelper dbHelper;
@@ -27,52 +28,8 @@ public class SplashScreen extends Activity{
         setContentView(R.layout.splash_screen_layout);
         dbHelper = new DBHelper(this);
         settings = PreferenceManager.getDefaultSharedPreferences(this);
-
-        if (settings.getString(Constants.SETTING_APP_SAVE_PATH_KEY, "empty").equals("empty")) {
-            if (Environment.getExternalStorageState().equals(Environment.MEDIA_UNMOUNTED)) {
-                settings.edit().putString(Constants.SETTING_APP_SAVE_PATH_KEY, Environment
-                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC)
-                        + File.separator + getString(R.string.app_name)).apply();
-            } else {
-                settings.edit().putString(Constants.SETTING_APP_SAVE_PATH_KEY, Environment
-                        .getExternalStorageDirectory().getAbsolutePath()
-                        + File.separator + getString(R.string.app_name)).apply();
-            }
-        }
-        appFolder = new File(settings.getString(Constants.SETTING_APP_SAVE_PATH_KEY,
-                Environment.getExternalStorageDirectory().getAbsolutePath()
-                        + File.separator + getString(R.string.app_name)));
-        if(!appFolder.exists()) {
-            try {
-                appFolder.mkdirs();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        dbHelper.addContactNumbersAndRecordsToDb(appFolder.getPath());
-        dbHelper.deleteNonExistingRecords();
-        dbHelper.deleteEmptyContacts();
-
-        int maxDays = Integer.parseInt(settings.getString(Constants.SETTING_DELETE_RECORDS_OLDER_THAN_KEY, "0"));
-        if (maxDays != 0) {
-            dbHelper.deleteRecordsOlderThan(GregorianCalendar.getInstance(), maxDays);
-        }
-
-        new Handler().postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                if (settings.getBoolean(Constants.SETTING_PASS_PROTECTION_KEY, false)) {
-                    Intent i = new Intent(getApplication(), PassActivity.class);
-                    startActivityForResult(i, PASS_REQUEST);
-                } else {
-                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-                    startActivity(intent);
-
-                    finish();
-                }
-            }
-        }, SPLASH_TIME_OUT);
+        DbTask task = new DbTask();
+        task.execute();
     }
 
     @Override
@@ -85,6 +42,62 @@ public class SplashScreen extends Activity{
             finish();
         } else {
             finish();
+        }
+    }
+
+    private class DbTask extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            if (settings.getString(Constants.SETTING_APP_SAVE_PATH_KEY, "empty").equals("empty")) {
+                if (Environment.getExternalStorageState().equals(Environment.MEDIA_UNMOUNTED)) {
+                    settings.edit().putString(Constants.SETTING_APP_SAVE_PATH_KEY, Environment
+                            .getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC)
+                            + File.separator + getString(R.string.app_name)).apply();
+                } else {
+                    settings.edit().putString(Constants.SETTING_APP_SAVE_PATH_KEY, Environment
+                            .getExternalStorageDirectory().getAbsolutePath()
+                            + File.separator + getString(R.string.app_name)).apply();
+                }
+            }
+            appFolder = new File(settings.getString(Constants.SETTING_APP_SAVE_PATH_KEY,
+                    Environment.getExternalStorageDirectory().getAbsolutePath()
+                            + File.separator + getString(R.string.app_name)));
+            if(!appFolder.exists()) {
+                try {
+                    appFolder.mkdirs();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            dbHelper.addContactNumbersAndRecordsToDb(appFolder.getPath());
+            dbHelper.deleteNonExistingRecords();
+            dbHelper.deleteEmptyContacts();
+            int maxDays = Integer.parseInt(settings.getString(Constants.SETTING_DELETE_RECORDS_OLDER_THAN_KEY, "0"));
+            if (maxDays != 0) {
+                dbHelper.deleteRecordsOlderThan(GregorianCalendar.getInstance(), maxDays);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    if (settings.getBoolean(Constants.SETTING_PASS_PROTECTION_KEY, false)) {
+                        Intent i = new Intent(getApplication(), PassActivity.class);
+                        startActivityForResult(i, PASS_REQUEST);
+                    } else {
+                        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                        startActivity(intent);
+
+                        finish();
+                    }
+                }
+            }, SPLASH_TIME_OUT);
         }
     }
 }
